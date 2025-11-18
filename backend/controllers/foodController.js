00e387be72a761796717cb8a9c6b2e8d596bb82b
Sharad Babar <sharadbabar2010@gmail.com>
2025-11-18 18:05:30 -0500

    Creates Admin Navbar

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -17,7 +17,7 @@ const addFood = async (req,res) => {
     })
     try {
         await food.save();
-        res.json({success:true,message:"Food Addes"})
+        res.json({success:true,message:"Food Added"})
     } catch (error) {
         console.log(error)
         res.json({success:false,message:"Error"})
@@ -25,4 +25,30 @@ const addFood = async (req,res) => {
 
 }
 
-export {addFood}
+//all food list
+const listFood= async (req,res)=>{
+    try {
+        const foods= await foodModel.find({});
+        res.json({success: true, data:foods})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})        
+    }
+}
+
+// remove food item
+
+const removeFood= async (req,res)=>{
+try {
+    const food=await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`,()=>{})
+
+    await foodModel.findByIdAndDelete(req.body.id)
+    res.json({success:true, message:"Food Removed"})
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
+}
+
+export {addFood,listFood,removeFood}
